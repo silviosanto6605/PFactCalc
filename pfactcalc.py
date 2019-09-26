5d8f9ef5f152ea4e3b4854f2da1970251dafd70a
Silvio Santoriello <silviosanto6605@gmail.com>
2019-09-26 21:58:11 +0200

    Pfactcalc 1.0

---
+++ pfactcalc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import random
+import math
+import os
+import subprocess
+import time
+
+
+
+number = int(input("Inserici il numero da scomporre in fattori primi: "))
+
+fattore = str(number)+"=1"
+divisor=2
+
+
+while number>=divisor:
+    if number % divisor == 0:
+        fattore=fattore+'*'+str(divisor)
+        number=number/divisor
+
+    else:
+        divisor=divisor+1
+
+    print(fattore)
